a98d03efdda8ca10125a0e2fb386fe07a4636870
Aleksandr Masalov <deksmes@bk.ru>
2025-11-02 14:21:23 +0300

    feat: add 1 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  let counter = 0;
+	for (let i = 0; i < matrix.length; i += 1) {
+		for (let j = 0; j < matrix.length; j += 1) {
+			if (matrix[i][j] === '^^') {
+				counter += 1;
+			}
+		}
+	}
+	return counter
 }
 
 module.exports = {
